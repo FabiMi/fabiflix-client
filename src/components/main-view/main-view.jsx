f76c732e434c38177c8b1ca58ca6a264d995c247
FabiMi <91958341+FabiMi@users.noreply.github.com>
2022-05-15 17:32:31 +0200

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import {MovieCard} from  './src/components/movie-card/movie-card.jsx';
 import {MovieView} from  './src/components/movie-view/movie-view.jsx';
+import { LoginView } from '../login-view/login-view.jsx';
 
 export class MainView extends React.Component {
 
@@ -9,12 +10,13 @@ export class MainView extends React.Component {
         super();
         this.state = {
           movies: [],
-          selectedMovie: null
+          selectedMovie: null,
+          user: null
         };
     }    
 
     componentDidMount(){
-      axios.get('http://fabiflix.herokuapp.com/moviess')
+      axios.get('http://fabiflix.herokuapp.com/movies')
       .then(response => { this.setState({movies: response.data}); })
     .catch(error => {console.log(error);
    });
@@ -26,12 +28,19 @@ export class MainView extends React.Component {
         });
       }
     
+    onLoggedIn(user) {
+      this.setState({
+        user
+      });
 
+    }
 
     render() {
         const { movies, selectedMovie } = this.state;
     
     
+        if (!user) return <LoginView onLoggedIn = {user => this.onLoggedIn(user)} /> 
+        
         if (movies.length === 0) return <div className="main-view" />
     
         return (
